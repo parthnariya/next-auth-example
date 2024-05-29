2d6125a78311d6a7e897ad9f8f34b97046ee7459
parthNariya <parthknariya1919@gmail.com>
2024-05-29 17:50:17 +0530

    util function for get user

--- actions/register.ts
+++ actions/register.ts
@@ -4,6 +4,7 @@ import { RegisterSchema } from "@/schemas/RegisterSchema";
 import { z } from "zod";
 import bcrypt from "bcrypt";
 import { prisma } from "@/lib/db";
+import { getUserByEmail } from "@/lib/utils";
 
 export async function register(values: z.infer<typeof RegisterSchema>) {
   const validationFields = RegisterSchema.safeParse(values);
@@ -15,11 +16,7 @@ export async function register(values: z.infer<typeof RegisterSchema>) {
 
   const { email, name, password } = validationFields.data;
 
-  const existingUser = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
+  const existingUser = await getUserByEmail(email);
   if (existingUser) {
     return {
       error: "Email already in use!!",
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,33 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { prisma } from "@/lib/db";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function getUserByEmail(email: string) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    return user;
+  } catch (e) {
+    return null;
+  }
+}
+
+export async function getUserById(id: string) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+    return user;
+  } catch (e) {
+    return null;
+  }
 }
