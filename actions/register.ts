b312b8e9872e33f2337d9c52c3ad6fabfc3b17f0
parthNariya <parthknariya1919@gmail.com>
2024-05-29 17:44:29 +0530

    registration

--- actions/register.ts
+++ actions/register.ts
@@ -2,15 +2,41 @@
 
 import { RegisterSchema } from "@/schemas/RegisterSchema";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import { prisma } from "@/lib/db";
 
 export async function register(values: z.infer<typeof RegisterSchema>) {
   const validationFields = RegisterSchema.safeParse(values);
-  if (!validationFields) {
+  if (!validationFields.data || validationFields.error) {
     return {
       error: "Invalid Fields",
     };
   }
+
+  const { email, name, password } = validationFields.data;
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (existingUser) {
+    return {
+      error: "Email already in use!!",
+    };
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
   return {
-    success: "Email Sent",
+    success: "User Created!",
   };
 }
